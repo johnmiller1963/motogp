0aa1040c02884bbfa95e21a3ee2550f2878da5b8
johnmiller1963 <john@ritmanager.co.uk>
2020-05-17 15:51:58 +0000

    carousel created

---
+++ assets/scripts/carousel.js
@@ -0,0 +1,10 @@
+$(function() {
+  'use strict';
+	$('.carousel .carousel-item[data-src]').each(function() {
+		var $this = $(this);
+
+		$this.prepend([
+			'<div style="background-image: url(', $this.attr('data-src'), ')"></div>'
+		].join(''));
+	});
+});
