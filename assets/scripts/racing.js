1bef3c4c57fe1106dd21e52bf7f7f5693f6df31e
johnmiller1963 <john@ritmanager.co.uk>
2020-05-21 18:00:55 +0000

    toggle results grid visibility without jumping

--- assets/scripts/racing.js
+++ assets/scripts/racing.js
@@ -4,4 +4,15 @@ function diff_years() {
     var diff =(dt2.getTime() - dt1.getTime()) / 1000;
     diff /= (60 * 60 * 24);
     document.getElementById("yearsSince1949").innerHTML = Math.abs(Math.round(diff/365.25));
- }
+ };
+
+ function toggleResults() {
+    var tableVisibility = document.getElementById("dataTable");
+        if (tableVisibility.style.display === "block") {
+            tableVisibility.style.display = "none";
+            document.getElementById("btnDisplayResults").innerText="View Race Results";
+        } else {
+            tableVisibility.style.display = "block";
+            document.getElementById("btnDisplayResults").innerText="Hide Race Results";
+        }
+ };
